36436bce829acf2fe42a5e2d3421932e20119ccd
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-10-21 01:51:29 +0200

    Refactored code correcting typos and spelling mistakes in code documentation.

--- Sec3_Classes/Classes.py
+++ Sec3_Classes/Classes.py
@@ -18,29 +18,34 @@ Guidelines:
   the PascalCase naming convention. For example: SavingsAccount.
 - The body of the class must be indented.
 
+Class definition:
+ Class definition the actual implementation of a class in a programming language.
+
 - Class has two main parts:  
-     - Header
-          Class definition. Specifies the name of the class and if it inherits
-          from another class
+     - Header 
+          - The first line in a class definition.
+          - Specifies the name of the class and if it inherits from another 
+            class.
+          - class header syntax: 
+             <keyword> <ClassName>:
+             class MyClass:
+
      - Body
           Contains the elements of the class, including the attributes and the
           behavior of the objects.
 
-     # class header
-     class NewClass:
-          # class body
-          # class attributes
-          # __init__()
+    
+     class NewClass:          # class header
+
+          # class attributes  # class body
+          # __init__() 
+          # methods - defines the action that the objects can perform.
           
      - The __init__() method is called automatically when an instance is 
        created to initialize the values of the attributes of the instance.
 
-
-
-  
           - pass keyword is used to fill the body of class/function.
      
-
 
 """
 print('')
@@ -50,13 +55,21 @@ class Backpack:
           self.color = color
           self.price = price
           self.weight = weight
-          self.backpack_details()
+     
+     def __str__(self):
+          return f'Backpack details:\nColor:\
+{self.color}\n\
+Price: {self.price}\n\
+Weight: {self.weight}'
 
-     def backpack_details(self):
-          """Backpack details"""
-          print(f'Backpack details:\nColor: {self.color}\nPrice: {self.price}\nWeight: {self.weight}')
+
+     # def backpack_details(self):
+     #      """Backpack details"""
+     #      print(f'Backpack details:\nColor: {self.color}\nPrice: {self.price}\nWeight: {self.weight}')
 
 
-travel_backpack = Backpack('Black', '$25')
+if __name__ == '__main__':
+     travel_backpack = Backpack('Black', '$25')
+     print(travel_backpack)
 
 

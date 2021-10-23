a291ed93928783131b019c2e92b79b9eab9a4003
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-10-23 21:36:15 +0200

    Completed Abstraction, Encapsulation, and Public-NonPublic chapters and exercises.

---
+++ Sec6_Encapsulation_Abstraction/encapsulation.py
@@ -0,0 +1,44 @@
+"""
+Encapsulation
+
+  - Encapsulation is the 'Bundling' of data and actions into a single unit the 
+    (class).
+  - The class acts like a capsule, that holds the attributes and the methods
+    of the object.
+  - The class separate the attributes and the methods from the rest of the
+    program.
+  - The class restricts access to the attributes and methods.
+   Applied through the principle of information hiding.
+  - The class restricts direct access to attributes in order to avoid making
+    potentially problematic changes to the state.
+  - We only let the rest of the program to see what we wanted to see from the 
+    class (to have access to the objects).
+  
+  - Getters and Setters
+  - Public and Non-Public
+
+
+Abstraction
+ - Show only the essential attributes and hide unnecessary details from the 
+   user. 
+ - Abstraction helps hide the complexity of the internal implementaton of the 
+   internal class from the user, For instance, we use our smart phones UI to 
+   access data, browse websites, and even do transactions. We don't need to 
+   understand the complex details and the algorithms happing behind the scenes.
+
+- A class has two main parts: Interface and implementation
+ - The interface is the visible part of the class that the program can interacts
+   with.  The wheel of the car is a good example of a class interface.
+ - The implementation is the internal part of the class with the code that 
+   performs the functionality behind the scenes. The car engine is a good 
+   example of a class implementation.
+
+ - Remember, in a class we choose what to show and what to hide.. public and 
+   non-public.
+ 
+ - Abstraction allows us to abstract common parts of the code to avoid 
+   repetition. Like creating a general class that can be inherited by other 
+   classes to prevent repetition of code.
+  
+
+"""
---
+++ Sec6_Encapsulation_Abstraction/public_nonPublic.py
@@ -0,0 +1,41 @@
+"""
+Public vs Non-Public Attributes
+
+ - In Python, attributes can be public and non-public.
+ - A public attribute can be accessed and modified directly without any access
+   restrictions.
+ - A non-public attribute is an attribute that shouldn't be accessed nor 
+   modified outside the class.
+ - In Python, we have two ways of defining a non-public attributes:
+    - By naming convection. preceding an attribute name with an _ 
+      _<attribute_name>  which will let other developers know that this 
+      attribute is non-public, so that attribute shouldn't be accessed or 
+      modified outside the class.
+    - By Name Mangling mechanism. Activating a mechanism that changes the 
+      attribute name and makes it more difficult to access. We can start the
+      attribute name by a double leading underscores __<attribute_name>.
+"""
+
+
+
+# Example of public attributes and non-public attributes
+
+class Car:
+  def __init__(self, brand, model, year):
+    self.brand = brand
+    self.model = model
+    self._year = year   # non-public attribute
+  
+  def __repr__(self):
+    return f"<Car(Brand: {self.brand}, Model: {self.model}, Year: {self._year})>"
+  
+
+car_1 = Car("Porsche", "911 Carrera", 2021)
+
+# We can have access to access and even modify instance attributes (public):
+car_1.model = "911"
+
+print(car_1)
+
+
+
